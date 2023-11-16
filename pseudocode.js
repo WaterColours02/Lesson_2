b64033732295f3dc0ce25f4654755ad8779fb292
hunter cheveldave <huntercheveldave@gmail.com>
2023-11-15 21:12:46 -0800

    first commit to development branch calculator.js bonus features work

---
+++ pseudocode.js
@@ -0,0 +1,61 @@
+// EXERCISE 1 
+// START 
+// SET number1 = GET number1 from user
+// SET number2 = GET number2 from user
+// PRINT number1 + number2
+
+
+
+
+//EXERCISE 2 
+// START
+// GET array of strings from user
+// SET newString = array.join('')
+// PRINT newString
+
+
+
+
+
+//EXERCISE 3
+// START
+// GET array of integers from user
+// WHILE iterator <= length of array
+// SET currentNumber = value of array at space iterator
+// IF currentNumber is odd
+// PUSH currentNumber to new array
+// ELSE move to next iteration  
+// PRINT new array
+
+
+
+
+
+//EXERCISE 4
+// START
+// GET string from user 
+// GET character from user
+// SET iterator = 0
+// SET characterCount = 0
+// WHILE iterator <= length of string
+//   SET currentCharacter = value of string at space iterator
+//   IF currentCharacter = character
+//     characterCount = characterCount + 1
+//   ELSE move to next iteration
+// PRINT characterCount
+
+
+
+
+//EXERCISE 5
+// START
+// Get 2 arrays of integers from user
+// SET iterator = 0
+// SET mergedArray = []
+// WHILE iterator <= length of array1
+//  push value of array2 at space iterator to mergedArray
+//  push value of array1 at space iterator to mergedArray
+//  iterator = iterator + 1
+// PRINT mergedArray
+
+
